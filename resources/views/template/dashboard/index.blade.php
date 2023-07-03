9e7d2fcdd4eacdaf76c024e1721127c8d2748814
alparahmansah <alparahmansah8@gmail.com>
2023-07-03 17:05:37 +0700

    update2

--- resources/views/template/dashboard/index.blade.php
+++ resources/views/template/dashboard/index.blade.php
@@ -6,26 +6,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <meta name="description" content=""/>
   <meta name="author" content=""/>
-  <title>Dashtreme Admin - Free Dashboard for Bootstrap 4 by Codervent</title>
+  <title>Understimate Dashboard</title>
   <!-- loader-->
-  <link href="assets/css/pace.min.css" rel="stylesheet"/>
-  <script src="assets/js/pace.min.js"></script>
+  <link href="{{ asset('template/css/pace.min.css') }}" rel="stylesheet"/>
+  <script src="{{ asset('template/js/pace.min.js') }}"></script>
   <!--favicon-->
-  <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="{{ asset('template/images/favicon.ico') }}" type="image/x-icon">
   <!-- Vector CSS -->
-  <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+  <link href="{{ asset('template/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet"/>
   <!-- simplebar CSS-->
-  <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+  <link href="{{ asset('template/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet"/>
   <!-- Bootstrap core CSS-->
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="{{ asset('template/css/bootstrap.min.css') }}" rel="stylesheet"/>
   <!-- animate CSS-->
-  <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <link href="{{ asset('template/css/animate.css') }}" rel="stylesheet" type="text/css"/>
   <!-- Icons CSS-->
-  <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+  <link href="{{ asset('template/css/icons.css') }}" rel="stylesheet" type="text/css"/>
   <!-- Sidebar CSS-->
-  <link href="assets/css/sidebar-menu.css" rel="stylesheet"/>
+  <link href="{{ asset('template/css/sidebar-menu.css') }}" rel="stylesheet"/>
   <!-- Custom Style-->
-  <link href="assets/css/app-style.css" rel="stylesheet"/>
+  <link href="{{ asset('template/css/app-style.css') }}" rel="stylesheet"/>
+  
   
 </head>
 
@@ -37,66 +38,59 @@
   <!--Start sidebar-wrapper-->
    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
      <div class="brand-logo">
-      <a href="index.html">
-       <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
-       <h5 class="logo-text">Dashtreme Admin</h5>
+      <a href="{{route('dashboard')}}">
+       <img src="{{ asset('template/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+       <h5 class="logo-text">Understimate Admin</h5>
      </a>
    </div>
    <ul class="sidebar-menu do-nicescrol">
       <li class="sidebar-header">MAIN NAVIGATION</li>
       <li>
-        <a href="index.html">
-          <i class="zmdi zmdi-view-dashboard"></i> <span>Dashboard</span>
+        <a href="#">
+          <i class="zmdi zmdi-case-check"></i> <span>Product</span>
         </a>
       </li>
 
       <li>
-        <a href="icons.html">
-          <i class="zmdi zmdi-invert-colors"></i> <span>UI Icons</span>
+        <a href="#">
+          <i class="zmdi zmdi-format-list-bulleted"></i> <span>Category-Product</span>
         </a>
       </li>
 
       <li>
-        <a href="forms.html">
-          <i class="zmdi zmdi-format-list-bulleted"></i> <span>Forms</span>
+        <a href="#">
+          <i class="zmdi zmdi-accounts-list"></i> <span>Buyers</span>
         </a>
       </li>
 
       <li>
-        <a href="tables.html">
-          <i class="zmdi zmdi-grid"></i> <span>Tables</span>
+        <a href="#">
+          <i class="zmdi zmdi-accounts-list"></i> <span>Sellers</span>
         </a>
       </li>
 
       <li>
-        <a href="calendar.html">
-          <i class="zmdi zmdi-calendar-check"></i> <span>Calendar</span>
-          <small class="badge float-right badge-light">New</small>
-        </a>
-      </li>
-
-      <li>
-        <a href="profile.html">
+        <a href="#">
           <i class="zmdi zmdi-face"></i> <span>Profile</span>
         </a>
       </li>
 
       <li>
-        <a href="login.html" target="_blank">
+        <a href="#" target="_blank">
           <i class="zmdi zmdi-lock"></i> <span>Login</span>
         </a>
       </li>
 
        <li>
-        <a href="register.html" target="_blank">
+        <a href="#" target="_blank">
           <i class="zmdi zmdi-account-circle"></i> <span>Registration</span>
         </a>
       </li>
 
-      <li class="sidebar-header">LABELS</li>
+      {{-- <li class="sidebar-header">LABELS</li>
       <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
       <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
-      <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li>
+      <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li> --}}
 
     </ul>
    
@@ -129,17 +123,10 @@
       <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();">
       <i class="fa fa-bell-o"></i></a>
     </li>
-    <li class="nav-item language">
-      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown" href="javascript:void();"><i class="fa fa-flag"></i></a>
-      <ul class="dropdown-menu dropdown-menu-right">
-          <li class="dropdown-item"> <i class="flag-icon flag-icon-gb mr-2"></i> English</li>
-          <li class="dropdown-item"> <i class="flag-icon flag-icon-fr mr-2"></i> French</li>
-          <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
-          <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
-        </ul>
-    </li>
+    
     <li class="nav-item">
       <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
+        
         <span class="user-profile"><img src="https://via.placeholder.com/110x110" class="img-circle" alt="user avatar"></span>
       </a>
       <ul class="dropdown-menu dropdown-menu-right">
@@ -148,7 +135,7 @@
            <div class="media">
              <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
             <div class="media-body">
-            <h6 class="mt-2 user-title">Sarajhon Mccoy</h6>
+              <h6 class="mt-2 user-title">Admin1</h6>
             <p class="user-subtitle">mccoy@example.com</p>
             </div>
            </div>
@@ -164,6 +151,7 @@
         <li class="dropdown-item"><i class="icon-power mr-2"></i> Logout</li>
       </ul>
     </li>
+    <h6 class="mt-2 user-title">Admin1</h6>
   </ul>
 </nav>
 </header>
@@ -175,7 +163,7 @@
 	
             <div class="content-wrapper">
               <div class="container-fluid">
-
+                @yield('content')
             </div>
 
 
@@ -243,24 +231,24 @@
 </div><!--End wrapper-->
 
 <!-- Bootstrap core JavaScript-->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="{{ asset('template/js/jquery.min.js') }}"></script>
+<script src="{{ asset('template/js/popper.min.js') }}"></script>
+<script src="{{ asset('template/js/bootstrap.min.js') }}"></script>
 
 <!-- simplebar js -->
-<script src="assets/plugins/simplebar/js/simplebar.js"></script>
+<script src="{{ asset('template/plugins/simplebar/js/simplebar.js') }}"></script>
 <!-- sidebar-menu js -->
-<script src="assets/js/sidebar-menu.js"></script>
+<script src="{{ asset('template/js/sidebar-menu.js') }}"></script>
 <!-- loader scripts -->
-<script src="assets/js/jquery.loading-indicator.js"></script>
+<script src="{{ asset('template/js/jquery.loading-indicator.js') }}"></script>
 <!-- Custom scripts -->
-<script src="assets/js/app-script.js"></script>
+<script src="{{ asset('template/js/app-script.js') }}"></script>
 <!-- Chart js -->
 
-<script src="assets/plugins/Chart.js/Chart.min.js"></script>
+<script src="{{ asset('template/plugins/Chart.js/Chart.min.js') }}"></script>
 
 <!-- Index js -->
-<script src="assets/js/index.js"></script>
+<script src="{{ asset('template/js/index.js') }}"></script>
 
 
 </body>
