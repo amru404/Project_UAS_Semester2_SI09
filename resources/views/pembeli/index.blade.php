adc69f6a366edd8e6846521b331351f08d7aa6c9
Alfarq <xiiipamuhammadalfaruq@gmail.com>
2023-07-15 02:08:04 +0700

    up

--- resources/views/pembeli/index.blade.php
+++ resources/views/pembeli/index.blade.php
@@ -177,7 +177,7 @@
 				<div class="row justify-content-center">
 					<div class="col-lg-6 text-center">
 						<div class="section-title">
-							<h1>Latest Products</h1>
+							<h1>New Arrival</h1>
 							<p>This ethalase contains a variety of our latest collections</p>
 						</div>
 					</div>
@@ -211,42 +211,6 @@
 			</div>
 		</div>
 		<!-- single product slide -->
-		<div class="single-product-slider">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-lg-6 text-center">
-						<div class="section-title">
-							<h1>Coming Products</h1>
-							<p>always look forward to our latest collection</p>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<!-- single product -->
-					<div class="col-lg-3 col-md-6">
-						<a href="{{route ('detail')}}" class="text-dark text-decoration-none">
-						<div class="card single-product p-1">
-							<img class="img-fluid" src="/assetuser/img/product/p6.jpg" alt="">
-							<div class="product-details ml-1">
-								<h6>addidas New Hammer sole
-									for Sports person</h6>
-								<div class="price">
-									<h6>$150.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-								<div class="prd-bottom">
-									<a href="" class="social-info">
-										<span class="ti-bag"></span>
-										<p class="hover-text">add to bag</p>
-									</a>
-								</div>
-							</div>
-						</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
 	</section>
 	<!-- end product Area -->
 
--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,56 +1,9 @@
 <header class="header_area sticky-header">
-<<<<<<< HEAD
-		<div class="main_menu">
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark main_box" style="border-radius:5px; color:white">
-				<div class="container">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<a class="navbar-brand logo_h" href="/pembeli"><img src="/assetuser/img/CEH.png" alt="" style="height: 55px;"></a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item  {{ (request()->is('pembeli')) ? 'active' : '' }}"><a class="nav-link" href="/pembeli">Home</a></li>
-							<li class="nav-item  {{ (request()->is('kategori')) ? 'active' : '' }}"><a class="nav-link" href="{{ route('kategori') }}">Category</a></li>
-							<li class="nav-item  {{ (request()->is('kontak')) ? 'active' : '' }}menu"><a class="nav-link" href="{{ route('kontak') }}">Contact</a></li>
-							
-							
-							
-							
-						</ul>
-						<ul class="nav navbar-nav navbar-right">
-
-							
-							<li class="nav-item"><a href="{{ route('cart') }}" class="cart"><span class="bi bi-cart3"></span></a></li>
-							<li class="nav-item">
-								<button class="search"><span class="lnr lnr-magnifier" id="search"></span></button>
-							</li>
-						<li class="nav-item submenu dropdown"><a href="{{ route('user.profile') }}" class="profile "><span class="bi bi-person-circle text-white"></span></a></li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</div>
-		<div class="search_input" id="search_input_box">
-			<div class="container">
-				<form class="d-flex justify-content-between">
-					<input type="text" class="form-control" id="search_input" placeholder="Search Here">
-					<button type="submit" class="btn"></button>
-					<span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
-				</form>
-			</div>
-		</div>
-	</header>
-=======
     <div class="main_menu">
-        <nav class="navbar navbar-expand-lg navbar-light main_box">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark main_box">
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <a class="navbar-brand logo_h" href="/pembeli"><img src="/assetuser/img/CEH.png" alt=""
+                <a class="navbar-brand logo_h" href="/pembeli"><img src="/assetuser/img/logo.png" alt=""
                         style="height: 55px" /></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -74,11 +27,11 @@
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li class="nav-item">
-                            <a href="{{ route('cart') }}" class="cart"><span class="bi bi-cart3"></span></a>
+                            <a href="{{ route('cart') }}" class="cart"><span class="bi bi-cart3 text-white"></span></a>
                         </li>
                         <li class="nav-item">
                             <button class="search">
-                                <span class="lnr lnr-magnifier" id="search"></span>
+                                <span class="lnr lnr-magnifier text-white" id="search"></span>
                             </button>
                         </li>
                         @guest
@@ -92,14 +45,14 @@
                         @else
                         <li class="nav-item submenu dropdown">
                             <a href="{{ route('user.profile') }}" class="profile">
-                                <span class="bi bi-person-circle"></span>
+                                <span class="bi bi-person-circle text-white"></span>
                             </a>
                         </li>
 
                         @if(Auth::user()->role == 'admin')
                         <li class="nav-item submenu dropdown">
                             <a href="{{ route('category') }}" class="profile">
-                                <button class="btn btn-warning mt-4">
+                                <button class="btn btn-warning mt-4 text-white">
                                     Admin
                                 </button>
                             </a>
@@ -155,4 +108,3 @@
         </div>
     </div>
 </header>
->>>>>>> 35a2a9e74e19bfdef50bd3adbef56994e454e952
