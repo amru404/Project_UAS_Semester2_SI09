c319c4b35e7a220653674ec403941b9a6e420800
Alfarq <xiiipamuhammadalfaruq@gmail.com>
2023-07-14 14:28:49 +0700

    up

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -3,102 +3,63 @@
         <nav class="navbar navbar-expand-lg navbar-light main_box">
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <a class="navbar-brand logo_h" href="/pembeli"
-                    ><img
-                        src="/assetuser/img/CEH.png"
-                        alt=""
-                        style="height: 55px"
-                /></a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <a class="navbar-brand logo_h" href="/pembeli"><img src="/assetuser/img/CEH.png" alt=""
+                        style="height: 55px" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div
-                    class="collapse navbar-collapse offset"
-                    id="navbarSupportedContent"
-                >
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                     <ul class="nav navbar-nav menu_nav ml-auto mr-5">
-                        <li
-                            class="nav-item {{ (request()->is('pembeli')) ? 'active' : '' }}"
-                        >
-                            <a class="nav-link" href="{{ route('index') }}"
-                                >Home</a
-                            >
+                        <li class="nav-item {{ (request()->is('pembeli')) ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('index') }}">Home</a>
                         </li>
-                        <li
-                            class="nav-item {{ (request()->is('kategori')) ? 'active' : '' }}"
-                        >
-                            <a class="nav-link" href="{{ route('kategori') }}"
-                                >Category</a
-                            >
+                        <li class="nav-item {{ (request()->is('kategori')) ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('kategori') }}">Category</a>
                         </li>
-                        <li
-                            class="nav-item {{ (request()->is('kontak')) ? 'active' : '' }}"
-                        >
-                            <a class="nav-link" href="{{ route('kontak') }}"
-                                >Contact</a
-                            >
+                        <li class="nav-item {{ (request()->is('kontak')) ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('kontak') }}">Contact</a>
                         </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li class="nav-item">
-                            <a href="{{ route('cart') }}" class="cart"
-                                ><span class="bi bi-cart3"></span
-                            ></a>
+                            <a href="{{ route('cart') }}" class="cart"><span class="bi bi-cart3"></span></a>
                         </li>
                         <li class="nav-item">
                             <button class="search">
-                                <span
-                                    class="lnr lnr-magnifier"
-                                    id="search"
-                                ></span>
+                                <span class="lnr lnr-magnifier" id="search"></span>
                             </button>
                         </li>
                         @guest
                         <li class="nav-item submenu dropdown">
-                            <a
-                                href="{{ route('user.profile') }}"
-                                class="profile"
-                            >
+                            <a href="{{ route('user.profile') }}" class="profile">
                                 <button class="btn btn-warning mt-4">
                                     Login
                                 </button>
                             </a>
                         </li>
-
                         @else
-						
-
                         <li class="nav-item submenu dropdown">
-                            <a
-                                href="{{ route('user.profile') }}"
-                                class="profile"
-                            >
-                                <span class="bi bi-person-circle" ></span>
+                            <a href="{{ route('user.profile') }}" class="profile">
+                                <span class="bi bi-person-circle"></span>
                             </a>
                         </li>
 
-						<li class="nav-item submenu dropdown">
-                            <a
-                                href="{{ route('category') }}"
-                                class="profile"
-                            >
+                        @if(Auth::user()->role == 'admin')
+                        <li class="nav-item submenu dropdown">
+                            <a href="{{ route('category') }}" class="profile">
                                 <button class="btn btn-warning mt-4">
                                     Admin
                                 </button>
                             </a>
                         </li>
+                        @endif
                         @endguest
+
 
                         <!-- <li class="nav-item submenu dropdown"><a href="{{ route('user.profile') }}" class="profile"><span>login</span></a></li>
 						<li class="nav-item submenu dropdown"><a href="{{ route('user.profile') }}" class="profile"><span class="bi bi-person-circle"></span></a></li> -->
@@ -140,20 +101,10 @@
     <div class="search_input" id="search_input_box">
         <div class="container">
             <form class="d-flex justify-content-between">
-                <input
-                    type="text"
-                    class="form-control"
-                    id="search_input"
-                    placeholder="Search Here"
-                />
+                <input type="text" class="form-control" id="search_input" placeholder="Search Here" />
                 <button type="submit" class="btn"></button>
-                <span
-                    class="lnr lnr-cross"
-                    id="close_search"
-                    title="Close Search"
-                ></span>
+                <span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
             </form>
         </div>
     </div>
 </header>
->>>>>>> b173090ace3e3d492419389f481f63f7e45018bf
